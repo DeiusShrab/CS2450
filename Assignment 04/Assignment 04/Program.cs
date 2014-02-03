c9939638406319aedb311c0262782359b7094bb7
DeiusShrab <deius_shrab@yahoo.com>
2014-02-03 14:12:52 -0700

    v0.1
    First Version

---
+++ Assignment 04/Assignment 04/Program.cs
@@ -0,0 +1,32 @@
+﻿/* CS 2450-002
+ * Jacob Christensen
+ * 
+ * I declare that the following code was written by me or provided
+ * by the instructor for this project. I understand that copying source
+ * code from any other source constitutes cheating, and that I will receive
+ * a zero on this project if I am found in violation of this policy
+ * 
+ * Looking online for inspiration is cool, but plagiarism is bad m'kay?
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Program Footer
+            Console.Write("\nPress ENTER to exit");
+            Console.ReadLine();
+        }
+    }
+}
